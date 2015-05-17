a3d7abe0464beaf7cb4042abfba04ab28a96f5d9
Hasan Karahan <hasan.karahan@blackhan.com>
2015-05-17 14:50:18 +0400

    DD-644: {before, after}-{show, hide} events

--- src/javascript/Main.js
+++ src/javascript/Main.js
@@ -18,8 +18,38 @@ Class("Accordion.Main", {
 
     methods: {
         initEvents: function () {
-            jQuery('.done-button').on('click', function () {
+            var $done = jQuery('.done-button');
+            $done.on('click', function () {
                 Accordion.Dizmo.showFront();
+            });
+
+            var $panels = jQuery('.dizmo-accordion-panel');
+            $panels.on('before-show', function (ev, do_show) {
+                var $target = jQuery(ev.target), id = $target.prop('id');
+                console.debug('[ON:BEF/SHOW-1] id=', id, $target);
+                do_show($target);
+            });
+            $panels.on('before-show', function (ev) {
+                var $target = jQuery(ev.target), id = $target.prop('id');
+                console.debug('[ON:BEF/SHOW-2] id=', id, $target);
+            });
+            $panels.on('after-show', function (ev) {
+                var $target = jQuery(ev.target), id = $target.prop('id');
+                console.debug('[ON:AFT/SHOW]-3 id=', id, $target);
+            });
+
+            $panels.on('before-hide', function (ev, do_hide) {
+                var $target = jQuery(ev.target), id = $target.prop('id');
+                console.debug('[ON:BEF/HIDE-1] id=', id, $target);
+                do_hide($target);
+            });
+            $panels.on('before-hide', function (ev) {
+                var $target = jQuery(ev.target), id = $target.prop('id');
+                console.debug('[ON:BEF/HIDE-2] id=', id, $target);
+            });
+            $panels.on('after-hide', function (ev) {
+                var $target = jQuery(ev.target), id = $target.prop('id');
+                console.debug('[ON:AFT/HIDE]-3 id=', id, $target);
             });
         }
     }
