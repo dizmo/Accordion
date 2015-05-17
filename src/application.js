704ed3492b36b74d9b45f252b7f046d8bd720b35
Hasan Karahan <hasan.karahan@blackhan.com>
2015-05-17 17:06:54 +0400

    DD-644: introduced `toggle` API

--- src/application.js
+++ src/application.js
@@ -10,5 +10,8 @@ function showFront() {
 
 var events = {};
 window.document.addEventListener('dizmoready', function () {
+    $acc = DizmoElements('.dizmo-accordion');
+    $p1 = jQuery('#P01'); $p2 = jQuery('#P02');
+
     MAIN = new Accordion.Main();
 });
