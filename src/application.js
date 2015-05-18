2fe338dd479b7b5bcf98452ee4c1dbc12f93b4c9
Hasan Karahan <hasan.karahan@blackhan.com>
2015-05-18 12:10:45 +0400

    DD-644: added 8th page

--- src/application.js
+++ src/application.js
@@ -11,7 +11,11 @@ function showFront() {
 var events = {};
 window.document.addEventListener('dizmoready', function () {
     $ACC = DizmoElements('.dizmo-accordion');
+
     $P1 = jQuery('#P01'); $P2 = jQuery('#P02');
+    $P3 = jQuery('#P03'); $P4 = jQuery('#P04');
+    $P5 = jQuery('#P05'); $P6 = jQuery('#P06');
+    $P7 = jQuery('#P07'); $P8 = jQuery('#P08');
 
     MAIN = new Accordion.Main();
 });
