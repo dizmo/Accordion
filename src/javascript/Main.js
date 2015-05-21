d70652242b07e2e48284bd8b8572f145411b546b
Hasan Karahan <hasan.karahan@blackhan.com>
2015-05-21 12:56:30 +0400

    DD-644: extended unlisted (nested) panel example

--- src/javascript/Main.js
+++ src/javascript/Main.js
@@ -18,6 +18,8 @@ Class("Accordion.Main", {
             );
 
             var $acc = jQuery('.dizmo-accordion');
+            $ACC = DizmoElements($acc);
+
             $P0 = $acc.find('.dizmo-accordion-panel:nth(0)');
             $P1 = $acc.find('.dizmo-accordion-panel:nth(1)');
             $P2 = $acc.find('.dizmo-accordion-panel:nth(2)');
@@ -29,11 +31,11 @@ Class("Accordion.Main", {
             $P8 = $acc.find('.dizmo-accordion-panel:nth(8)');
             $P9 = $acc.find('.dizmo-accordion-panel:nth(9)');
 
-            $PA = $acc.find('.dizmo-accordion-panel:nth(10)');
-            $PB = $acc.find('.dizmo-accordion-panel:nth(11)');
-            $PC = $acc.find('.dizmo-accordion-panel:nth(12)');
-            $ACC = DizmoElements($acc);
+            $PA = $acc.find('.dizmo-accordion-panel#a');
+            $PB = $acc.find('.dizmo-accordion-panel#b');
+            $PC = $acc.find('.dizmo-accordion-panel#c');
 
+            this.updateTimestamp($acc.find('div > span + p'));
             this.initEvents();
         }
     },
@@ -45,34 +47,114 @@ Class("Accordion.Main", {
                 Accordion.Dizmo.showFront();
             });
 
-            var $panels = jQuery('.dizmo-accordion-panel');
+            var $acc = jQuery('.dizmo-accordion'),
+                $panels = $acc.find('.dizmo-accordion-panel');
+
+            // accordion -- before- and after-show handlers
+
             $panels.on('before-show', function (ev, do_show) {
-                var $target = jQuery(ev.target), id = $target.prop('id');
-                console.debug('[ON:BEF/SHOW-1] id=', id, $target);
+                var $target = jQuery(ev.target);
+                console.debug('[ON:BEF/SHOW-1]', $target);
+
+                //
+                // If one ore more `before-show` handlers are listening then
+                // of these *exactly one* is required to invoke `do-show`: By
+                // doing so you can implement conditional logic if you indeed
+                // want to show the corresponding panel or not.
+                //
+
                 do_show($target);
+
             });
             $panels.on('before-show', function (ev) {
-                var $target = jQuery(ev.target), id = $target.prop('id');
-                console.debug('[ON:BEF/SHOW-2] id=', id, $target);
+                console.debug('[ON:BEF/SHOW-2]', jQuery(ev.target));
             });
             $panels.on('after-show', function (ev) {
-                var $target = jQuery(ev.target), id = $target.prop('id');
-                console.debug('[ON:AFT/SHOW]-3 id=', id, $target);
-            });
+                var $target = jQuery(ev.target);
+                console.debug('[ON:AFT/SHOW-3]', $target);
+                this.updateTimestamp($target.find('div > span + p'));
+            }.bind(this));
+
+            // accordion -- before- and after-hide handlers
 
             $panels.on('before-hide', function (ev, do_hide) {
-                var $target = jQuery(ev.target), id = $target.prop('id');
-                console.debug('[ON:BEF/HIDE-1] id=', id, $target);
+                var $target = jQuery(ev.target);
+                console.debug('[ON:BEF/HIDE-1]', $target);
+
+                //
+                // If one ore more `before-hide` handlers are listening then
+                // of these *exactly one* is required to invoke `do-hide`: By
+                // doing so you can implement conditional logic if you indeed
+                // want to hide the corresponding panel or not.
+                //
+
                 do_hide($target);
             });
             $panels.on('before-hide', function (ev) {
-                var $target = jQuery(ev.target), id = $target.prop('id');
-                console.debug('[ON:BEF/HIDE-2] id=', id, $target);
+                console.debug('[ON:BEF/HIDE-2]', jQuery(ev.target));
             });
             $panels.on('after-hide', function (ev) {
-                var $target = jQuery(ev.target), id = $target.prop('id');
-                console.debug('[ON:AFT/HIDE]-3 id=', id, $target);
+                console.debug('[ON:AFT/HIDE]-3', jQuery(ev.target));
             });
+
+            // accordion -- click handlers: next-switch between unlisted
+
+            var $panel_t,
+                $panel_n = $acc.find('.dizmo-accordion-panel' +
+                    ':not(#a):not(#b):not(#c)'),
+                $panel_a = $acc.find('.dizmo-accordion-panel#a'),
+                $panel_b = $acc.find('.dizmo-accordion-panel#b'),
+                $panel_c = $acc.find('.dizmo-accordion-panel#c');
+
+            $panel_n.find('div > span + p').on('click', function (ev) {
+                $panel_t = jQuery(ev.target).closest('.dizmo-accordion-panel');
+                DizmoElements($acc).daccordion('toggle-panel', $panel_a);
+            });
+            $panel_a.find('div > span + p').on('click', function () {
+                DizmoElements($acc).daccordion('toggle-panel', $panel_b);
+            });
+            $panel_b.find('div > span + p').on('click', function () {
+                DizmoElements($acc).daccordion('toggle-panel', $panel_c);
+            });
+            $panel_c.find('div > span + p').on('click', function () {
+                DizmoElements($acc).daccordion('toggle-panel', $panel_t);
+            });
+
+            // accordion -- click handlers: back-switch between unlisted
+
+            var $icon_a = $panel_a.find('.dizmo-accordion-panel-header-icon'),
+                $text_a = $panel_a.find('.dizmo-accordion-panel-header-text'),
+                $icon_b = $panel_b.find('.dizmo-accordion-panel-header-icon'),
+                $text_b = $panel_b.find('.dizmo-accordion-panel-header-text'),
+                $icon_c = $panel_c.find('.dizmo-accordion-panel-header-icon'),
+                $text_c = $panel_c.find('.dizmo-accordion-panel-header-text');
+
+            $icon_a.on('click', function () {
+                DizmoElements($acc).daccordion('toggle-panel', $panel_t);
+            });
+            $text_a.on('click', function () {
+                DizmoElements($acc).daccordion('toggle-panel', $panel_t);
+            });
+            $icon_b.on('click', function () {
+                DizmoElements($acc).daccordion('toggle-panel', $panel_a);
+            });
+            $text_b.on('click', function () {
+                DizmoElements($acc).daccordion('toggle-panel', $panel_a);
+            });
+            $icon_c.on('click', function () {
+                DizmoElements($acc).daccordion('toggle-panel', $panel_b);
+            });
+            $text_c.on('click', function () {
+                DizmoElements($acc).daccordion('toggle-panel', $panel_b);
+            });
+        },
+
+        updateTimestamp: function($timestamp) {
+            $timestamp.html(this.getTimestamp());
+        },
+
+        getTimestamp: function () {
+            return new Date().toLocaleString() + '&raquo;';
         }
     }
 });
